87a788d622a3d82f1f81457cff4b999382f4ade5
cjsdn9308 <cjsdn9308@naver.com>
2023-03-26 22:16:23 +0900

    초기 커밋.

---
+++ PDF_Read/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace PDF_Read
+{
+    partial class PDF_Read_Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnFileOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 392);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnFileOpen
+            // 
+            this.btnFileOpen.Location = new System.Drawing.Point(676, 410);
+            this.btnFileOpen.Name = "btnFileOpen";
+            this.btnFileOpen.Size = new System.Drawing.Size(111, 25);
+            this.btnFileOpen.TabIndex = 1;
+            this.btnFileOpen.Text = "열기";
+            this.btnFileOpen.UseVisualStyleBackColor = true;
+            // 
+            // PDF_Read_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFileOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "PDF_Read_Form";
+            this.Text = "PDF_Read";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnFileOpen;
+    }
+}
+
---
+++ PDF_Read/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+
+namespace PDF_Read
+{
+    public partial class PDF_Read_Form : Form
+    {
+        public PDF_Read_Form()
+        {
+            InitializeComponent();
+
+            this.btnFileOpen.Click += BtnFileOpen_Click;
+        }
+
+
+        private void BtnFileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();                   // File Open
+            fd.ShowDialog();
+
+            if(fd.FileName != "")                                       // File Name Check
+            {
+                string str = Ex(fd.FileName);
+                {
+                    string[] arr = str.Split("가1");                    // split
+                    string splstrF = arr[1].ToString().Substring(3);
+
+                    this.textBox1.Text = splstrF;                       // Result Input
+                }
+            }
+        }
+
+        private string Ex(string sPath)
+        {
+            StringBuilder result = new StringBuilder();
+            try
+            {
+                using (Stream newpdfStream = new FileStream(sPath, FileMode.Open, FileAccess.Read))
+                {
+                    PdfReader pdfReader = new PdfReader(newpdfStream);
+
+                    for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                    {
+                        result.Append(PdfTextExtractor.GetTextFromPage(pdfReader, i, new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy())).Append("\r\n\r\n\r\n");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result.ToString();
+
+        }
+    }
+}
